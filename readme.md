d1ca304a362338de8a0ee35be3add75003f4badc
Yuri Nesterov <nesteroviury@gmail.com>
2022-01-26 17:21:54 +0300

    base configuration added

---
+++ readme.md
@@ -0,0 +1,9 @@
+# bpm-bet-adviser
+
+## resources
+* https://github.com/camunda/camunda-bpm-examples/tree/master/spring-boot-starter/example-webapp
+
+## urls
+* [web app](http://localhost:8080)
+* [rest api](http://localhost:8080/engine-rest/engine)
+* [excamad](http://localhost:8080)
---
+++ src/main/java/ru/dev/study/bpm/bet/adviser/Application.java
@@ -0,0 +1,13 @@
+package ru.dev.study.bpm.bet.adviser;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
