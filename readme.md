953c3b7c361665af7c14bf19ddd22b8e5b46ce6d
Yuri Nesterov <nesteroviury@gmail.com>
2022-01-27 17:29:14 +0300

    excamad cors config, sample process added

--- readme.md
+++ readme.md
@@ -4,6 +4,9 @@
 * https://github.com/camunda/camunda-bpm-examples/tree/master/spring-boot-starter/example-webapp
 
 ## urls
+* [camunda](https://camunda.com/)
+* [camunda modeler](https://camunda.com/download/modeler/)
+* [excamad docker image](https://hub.docker.com/r/kotovdenis/excamad)
 * [web app](http://localhost:8080)
 * [rest api](http://localhost:8080/engine-rest/engine)
-* [excamad](http://localhost:8080)
+* [excamad](http://localhost:80)
---
+++ src/main/java/ru/dev/study/bpm/bet/adviser/configuration/ExcamadConfiguration.java
@@ -0,0 +1,27 @@
+package ru.dev.study.bpm.bet.adviser.configuration;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+//todo
+@Configuration
+public class ExcamadConfiguration {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> processCorsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
