8ac7ab6bd88f772d21e8114e1ff9645195507dbb
Yuri Nesterov <nesteroviury@gmail.com>
2022-02-01 12:33:36 +0300

    FirstFighterFormRetrieverDelegate changed

--- src/main/java/ru/dev/study/bpm/bet/adviser/delegate/FirstFighterFormRetrieverDelegate.java
+++ src/main/java/ru/dev/study/bpm/bet/adviser/delegate/FirstFighterFormRetrieverDelegate.java
@@ -3,20 +3,22 @@ package ru.dev.study.bpm.bet.adviser.delegate;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
+import ru.dev.study.bpm.bet.adviser.common.ContextVariables;
 
 import java.util.Random;
 
 @Component
 public class FirstFighterFormRetrieverDelegate implements JavaDelegate {
+    private static final Random RANDOM = new Random();
+
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        Random random = new Random();
-        int victoryCount = random.nextInt(29) + 1;
-        int defeatCount = random.nextInt(29) + 1;
+        int victoryCount = RANDOM.nextInt(29) + 1;
+        int defeatCount = RANDOM.nextInt(29) + 1;
 //        if(victoryCount+defeatCount > 30){
-            //todo: throw and catch exception
+        //todo: throw and catch exception
 //        }
-        double form = victoryCount / defeatCount * 100;
-        execution.setVariable();
+        double form = ((double) victoryCount) / defeatCount * 100;
+        execution.setVariable(ContextVariables.FIRST_FIGHTER_FORM, form);
     }
 }
