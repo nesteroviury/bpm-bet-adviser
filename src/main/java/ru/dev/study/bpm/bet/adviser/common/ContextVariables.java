ca3cef964dd142bc1ee2d2bbe1130303e0a39c8f
Yuri Nesterov <nesteroviury@gmail.com>
2022-02-01 12:11:27 +0300

    bpmn process changed

---
+++ src/main/java/ru/dev/study/bpm/bet/adviser/common/ContextVariables.java
@@ -0,0 +1,8 @@
+package ru.dev.study.bpm.bet.adviser.common;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class ContextVariables {
+    public static final String FIRST_FIGHTER_FORM = "firstFighterForm";
+}
