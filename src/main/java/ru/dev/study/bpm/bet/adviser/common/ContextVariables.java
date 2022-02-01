51e7242559c177a2688ce4cc2d699e889aa0757d
Yuri Nesterov <nesteroviury@gmail.com>
2022-02-01 12:32:47 +0300

    FirstFighterTablePlaceRetrieverDelegate added

--- src/main/java/ru/dev/study/bpm/bet/adviser/common/ContextVariables.java
+++ src/main/java/ru/dev/study/bpm/bet/adviser/common/ContextVariables.java
@@ -6,4 +6,5 @@ import lombok.NoArgsConstructor;
 public final class ContextVariables {
     public static final String FIRST_FIGHTER_FORM = "firstFighterForm";
     public static final String SECOND_FIGHTER_FORM = "secondFighterForm";
+    public static final String FIRST_FIGHTER_TABLE_PLACE = "firstFighterTablePlace";
 }
