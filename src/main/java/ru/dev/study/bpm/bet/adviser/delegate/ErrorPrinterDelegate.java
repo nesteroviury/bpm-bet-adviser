5045334b1c6339b86ac74ca94d138384b6ab23bf
Yuri Nesterov <nesteroviury@gmail.com>
2022-02-04 14:57:18 +0300

    ErrorPrinterDelegate added

---
+++ src/main/java/ru/dev/study/bpm/bet/adviser/delegate/ErrorPrinterDelegate.java
@@ -0,0 +1,16 @@
+package ru.dev.study.bpm.bet.adviser.delegate;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+import ru.dev.study.bpm.bet.adviser.common.ContextVariables;
+
+@Component
+@Slf4j
+public class ErrorPrinterDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        log.error("{}", execution.getVariable(ContextVariables.ERROR_MESSAGE));
+    }
+}
