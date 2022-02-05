e864f768ef0436b6c2972784a4ff06eb7ab5a839
Yuri Nesterov <nesteroviury@gmail.com>
2022-02-05 16:30:29 +0300

    ErrorMessageEventSenderDelegate added

---
+++ src/main/java/ru/dev/study/bpm/bet/adviser/delegate/ErrorMessageEventSenderDelegate.java
@@ -0,0 +1,14 @@
+package ru.dev.study.bpm.bet.adviser.delegate;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ErrorMessageEventSenderDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+    }
+}
