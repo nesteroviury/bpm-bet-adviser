b77ac5f7d6bb894cd85019d022f8b10298742c24
Yuri Nesterov <nesteroviury@gmail.com>
2022-02-05 18:02:39 +0300

    messaging support added

--- src/main/java/ru/dev/study/bpm/bet/adviser/Application.java
+++ src/main/java/ru/dev/study/bpm/bet/adviser/Application.java
@@ -3,9 +3,12 @@ package ru.dev.study.bpm.bet.adviser;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
 
-@SpringBootApplication
+@EnableBinding(Processor.class)
 @EnableProcessApplication
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
